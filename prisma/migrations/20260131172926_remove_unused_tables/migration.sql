70fbac96de4dc3b421577f64e3c39b8356fa2fc5
zlatanpham <zlatan.pham@gmail.com>
2026-02-01 00:30:43 +0700

    chore: remove unused database tables
    Remove 3 unused tables not referenced in codebase:
    - VietnameseHoliday
    - VietnameseZodiacInfo
    - VietnameseCulturalTip
    
    Migration generated to safely drop tables from PostgreSQL.

---
+++ prisma/migrations/20260131172926_remove_unused_tables/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `VietnameseCulturalTip` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VietnameseHoliday` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `VietnameseZodiacInfo` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "VietnameseCulturalTip";
+
+-- DropTable
+DROP TABLE "VietnameseHoliday";
+
+-- DropTable
+DROP TABLE "VietnameseZodiacInfo";
